3db26c41f7a0af9d3fbde5a26f0516ffd06541ae
severedsolo <severedsolo@users.noreply.github.com>
2019-06-16 13:50:16 +0100

    Initial commit

---
+++ ModLoaderExceptionCatcher.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using Enumerable = UniLinq.Enumerable;
+
+namespace ModLoaderExceptionCatcher
+{
+    using UnityEngine;
+    using System.IO;
+    using System.Collections.Generic;
+    using System;
+    
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class ModLoaderExceptionCatcher : MonoBehaviour
+    {
+        List<string> errors = new List<string>();
+        PopupDialog uiDialog;
+        Rect geometry = new Rect(0.5f,0.5f,600,10);
+        
+        private void Start()
+        {
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        string line = "None";
+            string path = KSPUtil.ApplicationRootPath + "KSP.log";
+            string newPath = path + "_backup";
+            File.Copy(path, newPath);
+            using (StreamReader reader = new StreamReader(newPath))
+            {;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Contains("Exception loading"))
+                    {
+                        errors.Add(line);
+                        Debug.Log("[MADLAD]: Found an error at " + line);
+                    }
+                }
+            }
+            Debug.Log("[MADLAD]: Found "+errors.Count()+" exceptions");
+            File.Delete(newPath);
+            if (errors.Count == 0) return;
+            uiDialog = GenerateDialog();
+            // the code that you want to measure comes here
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Debug.Log("[MADLAD]: Function executed in "+elapsedMs+" ms");
+        }
+
+        private PopupDialog GenerateDialog()
+        {
+             List<DialogGUIBase> dialog = new List<DialogGUIBase>();
+             for (int i = 0; i < errors.Count(); i++)
+             {
+                 string s = errors.ElementAt(i);
+                 dialog.Add(new DialogGUILabel(s, false, false));
+             }
+             dialog.Add(new DialogGUIButton("Close", CloseDialog, false));
+             return PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new MultiOptionDialog("ModLoaderDialog", "", "MADLAD - Assembly Exceptions", UISkinManager.defaultSkin, geometry, dialog.ToArray()), true, UISkinManager.defaultSkin);
+        }
+
+        void CloseDialog()
+        {
+            if(uiDialog != null) uiDialog.Dismiss();
+        }
+    }
+}
